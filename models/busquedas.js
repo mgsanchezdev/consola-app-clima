42b8de5251e846d8b66356627aed544719598cb6
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-21 16:48:56 -0300

    Configure axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,10 +1,23 @@
 const axios = require("axios");
 class Busquedas {
   constructor() {}
+  get paramsMapbox() {
+    return {
+      access_token: process.env.MAPBOX_KEY,
+      limit: 5,
+      language: "es",
+    };
+  }
   async ciudad(lugar = "") {
     try {
-      const respuesta = await axios.get("https://reqres.in/api/users?page=2");
-      console.log(respuesta.data);
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json?`,
+        params: this.paramsMapbox,
+      });
+
+      const resp = await instance.get();
+
+      console.log(resp.data);
       return [];
     } catch (error) {
       return [];
